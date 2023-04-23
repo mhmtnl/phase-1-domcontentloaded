b4a543d1729ba03b9cb39a2c43dd0601b94037ea
mehmetunlu <munlu4037@gmail.com>
2023-04-23 16:03:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").textContent = "This is really cool!"
+    console.log("DOM fully loaded and parsed");
+})
